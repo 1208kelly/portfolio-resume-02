3439aa364e2772fc946476f0ff01f51d248e7ded
1208kelly <1208kelly@gmail.com>
2020-11-09 17:41:51 +0000

    Adding text animate on scroll functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,29 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+//------- Text Slide In -------------------------------------------------------------------------------------------*/
+
+var element = document.getElementsByClassName("panel-header");
+element.addEventListener("animationstart", listener, false);
+element.addEventListener("animationend", listener, false);
+element.addEventListener("animationiteration", listener, false);
+
+element.className = "slide-left";
+
+function slideStart(event) {
+  var l = document.createElement("li");
+  switch(event.type) {
+    case "animationstart":
+      l.innerHTML = "Started: elapsed time is " + event.elapsedTime;
+      break;
+    case "animationend":
+      l.innerHTML = "Ended: elapsed time is " + event.elapsedTime;
+      break;
+    case "animationiteration":
+      l.innerHTML = "New loop started at time " + event.elapsedTime;
+      break;
+  }
+  document.getElementById("output").appendChild(l);
+}
